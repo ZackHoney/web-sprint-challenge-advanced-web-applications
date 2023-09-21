dc43b8de811853176c4d90b4ce8bb8bb882e5fdf
Zachary Honey <zhoney466@gmail.com>
2023-09-20 21:28:06 -0700

    Fixed my problem and completed

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -4,9 +4,8 @@ import Spinner from "./Spinner";
 
 // Import the Spinner component into this file and test
 // that it renders what it should for the different props it can take.
-
 test("sanity", () => {
-  expect(true).toBe(false);
+  expect(true).toBe(true);
 });
 
 test("Spinner renders correctly when on is true", () => {
